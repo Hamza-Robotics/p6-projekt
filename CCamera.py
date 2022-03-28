8a0dde0938bd5c3912ceb28dccaa2d41bd29e59f
Hamza <xamza1608@live.dk>
2022-03-28 14:19:58 +0200

    Udkast Til Segmentering Uden Filter

--- CCamera.py
+++ CCamera.py
@@ -32,29 +32,50 @@ def visualizer(rgb,Depth,hasrun=[]):
         plt.show()
         plt.pause(0.001)
 
+im_rgbd = rs.capture_frame(True, True)  # wait for frames and align them
+cam = o3d.camera.PinholeCameraIntrinsic()
+#cam.intrinsic_matrix=(  [231.7225,0,161.8237],[0,229.9110,80.4734],[0,0,1.0000])
+
+#intrinsic = o3d.camera.PinholeCameraIntrinsic(540, 960, 231.7225, 229.9110, 161.8237, 80.4734)
+#print("intrinics type",type(intrinsic)," data im:",type(im_rgbd.to_legeacy))
+intrinsic=(o3d.cpu.pybind.core.Tensor(np.array([[231.7225,0,161.8237],[0,229.9110,80.4734],[0,0,1.0000]])))
+
+pcd = o3d.t.geometry.PointCloud.create_from_rgbd_image(im_rgbd,intrinsic)
+#print(type(im_rgbd))
+pcd.transform([[-1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+
+
+#o3d.visualization.draw_geometries([pcd])
+#o3d.visualization.draw([pcd])
+#print(type(o3d.t.geometry.PointCloud.from_legacy_pointcloud(pcd)))
+
+
+
 
 
 while True:
+
     im_rgbd = rs.capture_frame(True, True)  # wait for frames and align them
-
-
-    #print(o3d.camera.PinholeCameraIntrinsic)
-    #pcd = o3d.geometry.PointCloud.create_from_rgbd_image( im_rgbd, o3d.camera.PinholeCameraIntrinsic(
-            #o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
-
-    
-    # Covert image into point cloud.
-    #pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-    #o3d.visualization.draw_geometries([pcd], zoom=0.5)
-    #Filtering
-
-
-    # Plane Object Segmentation
-
-    
-
+    pcd = o3d.t.geometry.PointCloud.create_from_rgbd_image(im_rgbd,intrinsic)
+    pcd=o3d.t.geometry.PointCloud.to_legacy(pcd)
+    plane_model, inliers = pcd.segment_plane(distance_threshold=0.01,
+                                         ransac_n=3,
+                                         num_iterations=1000)       
     # Features from each object 
-    visualizer(im_rgbd.depth,im_rgbd.color)
+    #visualizer(im_rgbd.depth,im_rgbd.color)
+    inlier_cloud = pcd.select_by_index(inliers)
+    inlier_cloud.paint_uniform_color([1.0, 0, 0])
+    outlier_cloud = pcd.select_by_index(inliers, invert=True)
+    o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud],
+                                    zoom=0.8,
+                                    front=[-0.4999, -0.1659, -0.8499],
+                                    lookat=[2.1813, 2.0619, 2.0999],
+                                    up=[0.1204, -0.9852, 0.1215])
+
+                                         
+    # Features from each object 
+    #visualizer(im_rgbd.depth,im_rgbd.color)
+
     if msvcrt.kbhit():
         if msvcrt.getwche() == 'c':
             break
