56d2eaf8e3de21acc15acdc11f1da718d5995f73
Hamza <xamza1608@live.dk>
2022-03-30 14:12:13 +0200

    Visualiseret data

--- UnpickleMeDickle.py
+++ UnpickleMeDickle.py
@@ -1,10 +1,19 @@
 import pickle
+import open3d as o3d
+import numpy as np
 
-pickle_file = 'C:\\Users\\SebBl\\source\\repos\\UnpickleMyDickle\\full_shape_val_data.pkl' ### Write path for the full_shape_val_data.pkl file ###
+
+pickle_file = "C:\\full-shape\\full_shape_train_data.pkl" ### Write path for the full_shape_val_data.pkl file ###
 
 with open(pickle_file, 'rb') as f:
     data = pickle.load(f)
 y = 0
-for x in data[0]['full_shape']['coordinate']:
-    print((data[0]['full_shape']['coordinate'][y]))
-    y+=1
+print(data[0]['full_shape']['coordinate'])
+
+for i in range(10):
+
+    xyz=np.asarray(data[i]['full_shape']['coordinate'])
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector((xyz))
+
+    o3d.visualization.draw_geometries([pcd])
