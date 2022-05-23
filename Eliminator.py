9d8a29becd4dabe8b9a87d98d684bcb725a6a7c2
Hamza <xamza1608@live.dk>
2022-05-23 20:19:55 +0200

    l

--- Eliminator.py
+++ Eliminator.py
@@ -2,15 +2,15 @@
 import pickle
 
 
-pickle_file = 'C:\\data_for_learning\\partial_val_data.pkl' ### Write path for the full_shape_val_data.pkl file ###
+pickle_file = 'D:\\data_for_learning\\partial_train_data.pkl' ### Write path for the full_shape_val_data.pkl file ###
 
 with open(pickle_file, 'rb') as f:
     data = pickle.load(f)
 
-
+print(len(data))
 objectlist=[]
 for i in range(len(data)):
-    if data[i]['semantic class'] == 'Knife' or data[i]['semantic class'] == 'Bottle'  or data[i]['semantic class'] == 'Bowl'  :
+    if data[i]['semantic class'] == 'Knife' or data[i]['semantic class'] == 'Bottle'  or data[i]['semantic class'] == 'Bowl' or data[i]['semantic class'] == 'Mug'  :
         objectlist.append(data[i])
 
 
